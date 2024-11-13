9f7025edbfc8c9a43574032cf4015eeb7aa9b36d
Evgeniy <cokrat@yandex.ru>
2024-11-13 16:54:32 +0300

    fix: chekstyle

--- main-service/src/main/java/ru/practicum/mainservice/util/Util.java
+++ main-service/src/main/java/ru/practicum/mainservice/util/Util.java
@@ -12,7 +12,7 @@ public class Util {
     public static final String TIME_ZONE_UTC = " UTC";
 
     public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern(DATE_TIME_PATTERN);
-    public static String INCOR_REQ = "Incorrectly made request.";
+    public static final String INCOR_REQ = "Incorrectly made request.";
 
     public static String compilationNotFound(Long compId) {
         return String.format("Compilation with id=%d not found", compId);
